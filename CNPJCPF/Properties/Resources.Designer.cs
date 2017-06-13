39c4e21f7294140ba1dff47aef50753b634de3cd
Júnior Pacheco <boloboca@hotmail.com>
2017-06-13 13:23:23 -0300

    Revert "Add link github"
    This reverts commit e0c5d85ea6acb3f6da51c54417537dcc5f4c0718.

--- CNPJCPF/Properties/Resources.Designer.cs
+++ CNPJCPF/Properties/Resources.Designer.cs
@@ -79,15 +79,5 @@ namespace CNPJCPF.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Github {
-            get {
-                object obj = ResourceManager.GetObject("Github", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
